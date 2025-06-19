f60ef98980a51220b2bfd17c3af4cd8fb3194f19
RAIDER02VT <federicosileoni892@gmail.com>
2025-06-19 17:50:41 +0200

    form

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI, Form
 import smtplib
 from email.message import EmailMessage
+import os
+
+email_user = os.getenv("EMAIL_USER")
+email_pass = os.getenv("EMAIL_PASS")
+email_to = os.getenv("EMAIL_TO")
 
 app = FastAPI()
 
@@ -12,14 +17,14 @@ def parse_form(
 ):
     # Costruisci l'email
     msg = EmailMessage()
-    msg["From"] = "federicosileoni892@gmail.com"
-    msg["To"] = "destinatario@tekval.it"
+    msg["From"] = email_user
+    msg["To"] = email_to
     msg["Subject"] = f"Nuovo messaggio dal sito da {email}"
     msg.set_content(f"{name}\n\nEmail: {email}\n\nMessaggio:\n{descrizione}")
 
     # Invia email (con SMTP SSL)
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-        smtp.login("federicosileoni892@gmail.com", "la-tua-password-app")
+        smtp.login(email_user, email_pass)
         smtp.send_message(msg)
 
     return {"message": "Messaggio inviato con successo!"}
